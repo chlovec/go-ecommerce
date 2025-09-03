4504888fbd0839f6d1a879c5ff068f2ed6a0dcdd
chlovec <alfred.okoronkwo@gmail.com>
2025-09-02 19:22:43 -0700

    model get all products

--- products/internal/data/products.go
+++ products/internal/data/products.go
@@ -97,3 +97,91 @@ func (p *ProductModel) GetByID(ctx context.Context, id int64) (*Product, error) 
 
 	return &product, nil
 }
+
+func (p *ProductModel) GetAll(ctx context.Context, filters Filters) ([]*Product, Metadata, error) {
+	builder := sq.Select(
+		"id",
+		"name",
+		"category_id",
+		"description",
+		"price",
+		"quantity",
+		"created_at",
+		"version",
+	).From("products")
+
+	builder = p.buildFilters(builder, filters)
+	query, args, _ := builder.ToSql()
+	rows, err := p.DB.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, Metadata{}, err
+	}
+	defer rows.Close()
+
+	products := []*Product{}
+	for rows.Next() {
+		var product Product
+		if err := rows.Scan(
+			&product.ID,
+			&product.Name,
+			&product.CategoryID,
+			&product.Description,
+			&product.Price,
+			&product.Quantity,
+			&product.CreatedAt,
+			&product.Version,
+		); err != nil {
+			return nil, Metadata{}, err
+		}
+		products = append(products, &product)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, Metadata{}, err
+	}
+
+	totalRecords, err := p.countProducts(ctx, filters)
+	if err != nil {
+		return nil, Metadata{}, err
+	}
+
+	metadata := calculateMetadata(int(totalRecords), filters.Page, filters.PageSize)
+	return products, metadata, nil
+}
+
+func (p *ProductModel) buildFilters(builder sq.SelectBuilder, filters Filters) sq.SelectBuilder {
+	if len(filters.IDs) > 0 {
+		builder = builder.Where(sq.Eq{"ids": filters.IDs})
+	}
+	if filters.Name != "" {
+		builder = builder.Where(
+			"to_tsvector('simple', name) @@ plainto_tsquery('simple', ?)",
+			filters.Name,
+		)
+	}
+	if filters.DateFrom != nil {
+		builder = builder.Where(sq.GtOrEq{"created_at": filters.DateFrom})
+	}
+	if filters.DateFrom != nil {
+		builder = builder.Where(sq.LtOrEq{"created_at": filters.DateTo})
+	}
+
+	builder = builder.OrderBy(filters.sortColumns())
+	builder = builder.Limit(uint64(filters.PageSize)).Offset(uint64(filters.offset()))
+	return builder
+}
+
+func (p *ProductModel) countProducts(ctx context.Context, filters Filters) (int64, error) {
+	builder := sq.Select("COUNT(*)").From("products")
+	builder = p.buildFilters(builder, filters)
+
+	query, args, _ := builder.ToSql()
+
+	var totalRecords int64
+	err := p.DB.QueryRowContext(ctx, query, args...).Scan(&totalRecords)
+	if err != nil {
+		return 0, err
+	}
+
+	return totalRecords, nil
+}
--- products/internal/data/products_test.go
+++ products/internal/data/products_test.go
@@ -200,3 +200,240 @@ func TestProductModel_GetById(t *testing.T) {
 		assert.Nil(t, actualProduct)
 	})
 }
+
+func TestProductModel_GetAll(t *testing.T) {
+	t.Parallel()
+
+	db, sqlMock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	productModel := ProductModel{DB: db}
+	ctx := context.Background()
+
+	var mockQuery = regexp.QuoteMeta(`
+		SELECT id, name, category_id, description, price, quantity, created_at, version
+		FROM products
+		ORDER BY id ASC LIMIT 20 OFFSET 0
+	`)
+
+	filters := Filters{
+		Page:     1,
+		PageSize: 20,
+	}
+	createdAt := time.Date(2023, time.July, 1, 10, 0, 0, 0, time.UTC)
+
+	t.Run("fetch all products with filters successfully", func(t *testing.T) {
+		testFilters := Filters{
+			Page:     1,
+			PageSize: 2,
+			IDs:      []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			Name:     "test",
+			DateFrom: &createdAt,
+			DateTo:   &createdAt,
+			Sorts:    []string{"-created_at", "name", "id"},
+		}
+		expectedProducts := []*Product{
+			{
+				ID:          1,
+				Name:        "Test Product1",
+				CategoryID:  999,
+				Description: "Test product1 description",
+				Price:       10.99,
+				Quantity:    5,
+				CreatedAt:   createdAt,
+				Version:     1,
+			},
+			{
+				ID:          13,
+				Name:        "Test Product2",
+				CategoryID:  12,
+				Description: "Test product2 description",
+				Price:       25.73,
+				Quantity:    16,
+				CreatedAt:   createdAt,
+				Version:     1,
+			},
+		}
+		expectedMetadata := Metadata{
+			CurrentPage:  1,
+			PageSize:     2,
+			FirstPage:    1,
+			LastPage:     5,
+			TotalRecords: 10,
+		}
+
+		mockRow := sqlMock.NewRows(
+			[]string{
+				"id",
+				"name",
+				"category_id",
+				"description",
+				"price",
+				"quantity",
+				"created_at",
+				"version",
+			},
+		).
+			AddRow(1, "Test Product1", 999, "Test product1 description", 10.99, 5, createdAt, 1).
+			AddRow(13, "Test Product2", 12, "Test product2 description", 25.73, 16, createdAt, 1)
+
+		testQuery := regexp.QuoteMeta(
+			`
+			SELECT id, name, category_id, description, price, quantity, created_at, version
+			FROM products
+			WHERE ids IN (?,?,?,?,?,?,?,?,?,?) 
+				AND to_tsvector('simple', name) @@ plainto_tsquery('simple', ?)
+				AND created_at >= ? 
+				AND created_at <= ? 
+			ORDER BY created_at DESC, name ASC, id ASC
+			LIMIT 2 OFFSET 0`,
+		)
+		sqlMock.ExpectQuery(testQuery).WithArgs().WillReturnRows(mockRow)
+
+		countQuery := regexp.QuoteMeta(
+			`SELECT COUNT(*) 
+			FROM products 
+			WHERE ids IN (?,?,?,?,?,?,?,?,?,?) 
+				AND to_tsvector('simple', name) @@ plainto_tsquery('simple', ?) 
+				AND created_at >= ? AND created_at <= ? 
+			ORDER BY created_at DESC, name ASC, id ASC 
+			LIMIT 2 OFFSET 0`,
+		)
+		countRows := sqlmock.NewRows([]string{"count"}).AddRow(10)
+		sqlMock.ExpectQuery(countQuery).WithArgs().WillReturnRows(countRows)
+
+		actualProducts, metadata, err := productModel.GetAll(ctx, testFilters)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedProducts, actualProducts)
+		assert.Equal(t, expectedMetadata, metadata)
+	})
+
+	t.Run("no rows returned", func(t *testing.T) {
+		mockRow := sqlMock.NewRows(
+			[]string{
+				"id",
+				"name",
+				"category_id",
+				"description",
+				"price",
+				"quantity",
+				"created_at",
+				"version",
+				"add_col",
+			},
+		)
+		sqlMock.ExpectQuery(mockQuery).WithArgs().WillReturnRows(mockRow)
+
+		countQuery := regexp.QuoteMeta(
+			`SELECT COUNT(*) FROM products ORDER BY id ASC LIMIT 20 OFFSET 0`,
+		)
+		countRows := sqlmock.NewRows([]string{"count"}).AddRow(0)
+		sqlMock.ExpectQuery(countQuery).WithArgs().WillReturnRows(countRows)
+
+		actualProducts, metadata, err := productModel.GetAll(ctx, filters)
+		assert.NoError(t, err)
+		assert.Equal(t, []*Product{}, actualProducts)
+		assert.Equal(t, Metadata{}, metadata)
+	})
+
+	t.Run("execute query error", func(t *testing.T) {
+		sqlMock.ExpectQuery(mockQuery).WithArgs().WillReturnError(errors.New("execute query error"))
+
+		actualProducts, metadata, err := productModel.GetAll(ctx, filters)
+		assert.Error(t, err)
+		assert.Equal(t, err.Error(), "execute query error")
+		assert.Nil(t, actualProducts)
+		assert.Equal(t, Metadata{}, metadata)
+	})
+
+	t.Run("row scan error", func(t *testing.T) {
+		mockRow := sqlMock.NewRows(
+			[]string{
+				"id",
+				"name",
+				"category_id",
+				"description",
+				"price",
+				"quantity",
+				"created_at",
+				"version",
+				"add_col",
+			},
+		).
+			AddRow(1, "Test Product", 999, "A test product", 10.99, 5, createdAt, 1, 10)
+		sqlMock.ExpectQuery(mockQuery).WithArgs().WillReturnRows(mockRow)
+
+		actualProducts, metadata, err := productModel.GetAll(ctx, filters)
+		assert.Error(t, err)
+		assert.Equal(t, err.Error(), "sql: expected 9 destination arguments in Scan, not 8")
+		assert.Nil(t, actualProducts)
+		assert.Equal(t, Metadata{}, metadata)
+	})
+
+	t.Run("row error", func(t *testing.T) {
+		mockRow := sqlMock.NewRows(
+			[]string{
+				"id",
+				"name",
+				"category_id",
+				"description",
+				"price",
+				"quantity",
+				"created_at",
+				"version",
+			},
+		).
+			AddRow(1, "Test Product", 999, "A test product", 10.99, 5, createdAt, 1).
+			RowError(0, errors.New("rows iteration error"))
+		sqlMock.ExpectQuery(mockQuery).WithArgs().WillReturnRows(mockRow)
+
+		actualProducts, metadata, err := productModel.GetAll(ctx, filters)
+		assert.Error(t, err)
+		assert.Equal(t, err.Error(), "rows iteration error")
+		assert.Nil(t, actualProducts)
+		assert.Equal(t, Metadata{}, metadata)
+	})
+
+	t.Run("count product error", func(t *testing.T) {
+		mockRow := sqlMock.NewRows(
+			[]string{
+				"id",
+				"name",
+				"category_id",
+				"description",
+				"price",
+				"quantity",
+				"created_at",
+				"version",
+				"add_col",
+			},
+		)
+		sqlMock.ExpectQuery(mockQuery).WithArgs().WillReturnRows(mockRow)
+
+		countQuery := regexp.QuoteMeta(
+			`SELECT COUNT(*) FROM products ORDER BY id ASC LIMIT 20 OFFSET 0`,
+		)
+		sqlMock.ExpectQuery(countQuery).WithArgs().WillReturnError(errors.New("sql error"))
+
+		actualProducts, metadata, err := productModel.GetAll(ctx, filters)
+		assert.Error(t, err)
+		assert.Equal(t, "sql error", err.Error())
+		assert.Nil(t, actualProducts)
+		assert.Equal(t, Metadata{}, metadata)
+	})
+
+	t.Run("context canceled", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		mockError := errors.New("db error")
+		sqlMock.ExpectQuery(mockQuery).WithArgs(23).WillReturnError(mockError)
+
+		actualProducts, metadata, err := productModel.GetAll(ctx, filters)
+		assert.Error(t, err)
+		assert.Equal(t, err.Error(), "context canceled")
+		assert.Nil(t, actualProducts)
+		assert.Equal(t, Metadata{}, metadata)
+	})
+}
